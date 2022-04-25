84e754da69385677328b42d8186d0b2f3ca710c5
Juan Tous <tousjuan01@gmail.com>
2022-04-25 18:22:55 -0500

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -44,6 +48,10 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
     MatDialogModule
   ],
   providers: [DishService, LeaderService],
