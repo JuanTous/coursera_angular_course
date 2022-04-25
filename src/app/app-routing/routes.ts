12193a306f63e9b55a4a4932eb55628629354313
Juan Tous <tousjuan01@gmail.com>
2022-04-25 17:24:30 -0500

    Single Page Applications Part 3

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -3,11 +3,13 @@ import { MenuComponent } from '../menu/menu.component';
 import { HomeComponent } from '../home/home.component';
 import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
 import { ContactComponent } from '../contact/contact.component';
+import { AboutComponent } from '../about/about.component';
 
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
   { path: 'contactus',  component: ContactComponent},
   { path: 'dishdetail/:id',     component: DishdetailsComponent },
+  { path: 'aboutus',     component: AboutComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
